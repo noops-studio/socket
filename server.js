1f1f5f1c7f4e607814bf1a97ca6f627af170cb60
beery <beeryba05@gmail.com>
2024-12-03 09:16:31 +0200

    Initialize Socket.IO project with server setup, HTML clients, and package configuration

---
+++ server.js
@@ -0,0 +1,81 @@
+import express from 'express';
+import http from 'http';
+import { Server as socketIo } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+const server = http.createServer(app);
+const io = new socketIo(server, {
+  cors: {
+    origin: '*',
+    methods: ['POST']
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const clients = {};
+
+// app.get('/', async (req, res) => {
+//   res.status(200).send({ res: 'ok' });
+// });
+
+app.post('/sendmsg', (req, res) => {
+  const { tel, ext, grp, company } = req.body;
+  const socketId = clients[`${company}${ext}`];
+
+  if (socketId) {
+    console.log(req.body);
+    io.to(socketId).emit('message', tel);
+    res.status(200).send({ status: 'Message sent' });
+  } else {
+    res.status(404).send({ status: 'User not connected' });
+  }
+});
+
+
+
+
+
+
+io.on('connection', (socket) => {
+  socket.on('register', (userid) => {
+    if (clients[userid]) {
+      clients[userid].push(socket.id);
+    } else {
+      clients[userid] = [socket.id];
+    }
+    console.log(`User ${userid} registered with socket ID ${socket.id}`);
+  });
+
+  socket.on('disconnect', () => {
+    for (let userid in clients) {
+      const index = clients[userid].indexOf(socket.id);
+      if (index !== -1) {
+        clients[userid].splice(index, 1);
+        if (clients[userid].length === 0) {
+          delete clients[userid];
+        }
+        break;
+      }
+    }
+    console.log(`A client disconnected: ${socket.id}`);
+  });
+});
+
+function notifyUser(userid, message) {
+  if (clients[userid]) {
+    clients[userid].forEach(socketId => {
+      io.to(socketId).emit('notification', message);
+    });
+  }
+}
+
+
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
